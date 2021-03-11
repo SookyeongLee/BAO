2573c795fabee0b3669eda58a02770cda9111da9
e <e@192.168.0.10>
2021-03-11 13:09:18 +0900

    222

--- Mohel/src/main/java/spring/bao/mapper/AuthenticationIf.java
+++ Mohel/src/main/java/spring/bao/mapper/AuthenticationIf.java
@@ -3,19 +3,16 @@ package spring.bao.mapper;
 
 import java.util.ArrayList;
 
-import com.google.gson.JsonElement;
-
 import spring.bao.beans.MemberBean;
 import spring.bao.beans.RequestBean;
 
-public interface AuthenticationIF {
+public interface AuthenticationIf {
 
 	public int isMember(MemberBean memberbean);
 	public int insMember(MemberBean memberbean);	
 	public int isAccess(MemberBean memberbean);
 	public ArrayList<RequestBean> getRecentList(RequestBean req);
 	public int insAccess(MemberBean member);
-	
 
 
 	
